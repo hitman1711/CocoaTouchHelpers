2bbff58cb7aca8e419cdaedc663c128ce7d31cfa
Felipe Augusto <felipowsky@gmail.com>
2015-06-14 21:25:24 -0300

    added cocoa touch helpers header

---
+++ CocoaTouchHelpers/CocoaTouchHelpers/CocoaTouchHelpers.h
@@ -0,0 +1,13 @@
+//
+//  CocoaTouchHelpers.h
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "UIViewController+CTH.h"
+#import "UIApplication+CTH.h"
+#import "NSString+CTH.h"
+#import "NSData+CTH.h"
+#import "UIColor+CTH.h"
+#import "UIImageView+CTH.h"
+#import "CTHFunctions.h"
