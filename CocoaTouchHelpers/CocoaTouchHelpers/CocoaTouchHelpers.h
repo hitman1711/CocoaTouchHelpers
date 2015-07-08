c2583287d6b26ac87341ed79b0dc9e2041c383e2
Felipe Augusto <felipowsky@gmail.com>
2015-07-08 11:04:18 -0300

    added nsformatter category in main header

--- CocoaTouchHelpers/CocoaTouchHelpers/CocoaTouchHelpers.h
+++ CocoaTouchHelpers/CocoaTouchHelpers/CocoaTouchHelpers.h
@@ -10,6 +10,7 @@
 #import "NSString+CTH.h"
 #import "NSData+CTH.h"
 #import "NSDate+CTH.h"
+#import "NSDateFormatter+CTH.h"
 #import "UIColor+CTH.h"
 #import "UIImageView+CTH.h"
 #import "Reachability+CTH.h"
